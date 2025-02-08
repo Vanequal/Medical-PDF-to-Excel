f35638f73f335de98765bcd66827a95db0b4ce00
Vanequal <adgarovzafar21@gmail.com>
2025-02-09 00:51:34 +0500

    Добавлен прогресс-бар для обработки файлов

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/loginсв" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
           path="/dashboard"
