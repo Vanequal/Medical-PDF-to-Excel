5253bbef3df63ff9975d7df82bac983e46657e39
Vanequal <adgarovzafar21@gmail.com>
2025-02-10 00:47:54 +0500

    Vercel

--- frontend/src/components/utils/UploadPDF.js
+++ frontend/src/components/utils/UploadPDF.js
@@ -33,19 +33,20 @@ const UploadPDF = () => {
 
   const handleFileUpload = async (files) => {
     if (files.length === 0) return;
-
+  
     const formData = new FormData();
     files.forEach((file) => formData.append("files", file));
-
+  
     try {
       setLoading(true);
       setError(null);
-
-      const response = await fetch("http://127.0.0.1:8000/upload/", {
+  
+      // Используй относительный путь к бэкенду
+      const response = await fetch("/api/upload/", {
         method: "POST",
         body: formData,
       });
-
+  
       if (response.ok) {
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
@@ -65,6 +66,7 @@ const UploadPDF = () => {
       setLoading(false);
     }
   };
+  
 
   return (
     <div className="p-6">
