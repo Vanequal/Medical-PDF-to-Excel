f8f979f8f84f16e2e813d1a5721908d31c75bbeb
Vanequal <adgarovzafar21@gmail.com>
2025-02-11 02:21:44 +0500

    deploy

--- frontend/src/components/Common/Navbar.js
+++ frontend/src/components/Common/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiGrid, FiUser } from "react-icons/fi"; // Подключаем иконки из react-icons
+import { User } from 'lucide-react';
+import { Grid2x2 } from 'lucide-react';
 import "../../styles/navbar.css";
 
 const Navbar = () => {
@@ -12,13 +13,13 @@ const Navbar = () => {
             <ul className="navbar-links">
                 <li>
                     <Link to="/dashboard" className="navbar-icon">
-                        <FiGrid />
+                    <Grid2x2 />
                         <span className="navbar-tooltip">Dashboard</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/login" className="navbar-icon">
-                        <FiUser />
+                        <User />
                         <span className="navbar-tooltip">Login</span>
                     </Link>
                 </li>
